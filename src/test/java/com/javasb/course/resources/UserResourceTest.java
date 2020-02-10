1239d903e25a1b270aa70390ca8a604e019c2d2e
Bruno Monteiro <brunobapmon@gmail.com>
2020-02-10 20:18:17 -0300

    h2 db, test profile, jpa

---
+++ src/test/java/com/javasb/course/resources/UserResourceTest.java
@@ -0,0 +1,13 @@
+package com.javasb.course.resources;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@ActiveProfiles("test")
+@SpringBootTest
+public class UserResourceTest {
+
+}
