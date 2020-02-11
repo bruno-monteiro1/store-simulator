355fed6900e061eb700fc12d22867ec0c9b1f377
Bruno Monteiro <brunobapmon@gmail.com>
2020-02-10 21:00:04 -0300

    JPA rep, dep injection, db seeding

--- src/main/java/com/javasb/course/entities/User.java
+++ src/main/java/com/javasb/course/entities/User.java
@@ -13,7 +13,7 @@ public class User implements Serializable{
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
+	private Long id;
 	private String name;
 	private String email;
 	private String phone;
@@ -22,7 +22,7 @@ public class User implements Serializable{
 	public User() {
 	}
 
-	public User(long id, String name, String email, String phone, String password) {
+	public User(Long id, String name, String email, String phone, String password) {
 		this.id = id;
 		this.name = name;
 		this.email = email;
---
+++ src/main/java/com/javasb/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.javasb.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.javasb.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
