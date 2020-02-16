d5f8e7508ab66445fac705006edf1ddd01dfb6e2
Bruno Monteiro <brunobapmon@gmail.com>
2020-02-16 13:09:44 -0300

    added product class

---
+++ src/main/java/com/javasb/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.javasb.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.javasb.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
