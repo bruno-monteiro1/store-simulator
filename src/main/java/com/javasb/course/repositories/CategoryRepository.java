a2fb865f58fc71e899bd3ba6ff313d4c79b97adb
Bruno Monteiro <brunobapmon@gmail.com>
2020-02-16 12:41:38 -0300

    added categories

---
+++ src/main/java/com/javasb/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.javasb.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.javasb.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
